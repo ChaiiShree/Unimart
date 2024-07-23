2bf04f2998b5ab8cff03efbc0f635a523563bb7f
Chaitanya Jayant <sycothic11@gmail.com>
2024-07-23 22:24:10 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://uniipal-frontend.vercel.app', // Replace with your frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
