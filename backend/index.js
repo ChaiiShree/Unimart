c37375ad9458be2c23e37297f1c6460a95077d0f
Chaitanya Jayant <sycothic11@gmail.com>
2024-07-24 07:32:19 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = ['https://uniipal-frontend.vercel.app',"https://uniipal.vercel.app","http://localhost:5173/"];
+
 app.use(cors({
-  origin: 'https://uniipal-frontend.vercel.app', // Replace with your frontend URL
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
 
 app.use(bodyParser.json({ limit: '5mb' }));
