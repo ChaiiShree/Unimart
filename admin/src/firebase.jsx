9974d094747c9351d75455a26372383c9b22c857
ChaiiShree <sycothic11@gmail.com>
2024-09-13 10:11:23 +0530

    admin_go

--- admin/src/firebase.jsx
+++ admin/src/firebase.jsx
@@ -1,22 +1,24 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 // Use Vite's import.meta.env for Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyB-on-CzoEi8ingMKT0Jk-5WngmQSlYDPo",
-  authDomain: "unipal-af10a.firebaseapp.com",  
-  projectId: "unipal-af10a",  
-  storageBucket: "unipal-af10a.appspot.com",  
-  messagingSenderId: "821015065291",  
-  appId: "1:821015065291:web:cc9881bc7fe4410fa1a14e",  
-  measurementId: "G-YJ6LS127YG"
-  };
+
+  apiKey: "AIzaSyANtMPzEO6lEQ9DB79JPnztOoKdtHY6LYc",
+  authDomain: "unimart-d57bc.firebaseapp.com",
+  databaseURL: "https://unimart-d57bc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "unimart-d57bc",
+  storageBucket: "unimart-d57bc.appspot.com",
+  messagingSenderId: "553313828640",
+  appId: "1:553313828640:web:501ace36850ac04d922145",
+  measurementId: "G-327YKHYJRM"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); // Only call this if you are using Firebase Analytics
+// const analytics = getAnalytics(app); // Only call this if you are using Firebase Analytics
 const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
